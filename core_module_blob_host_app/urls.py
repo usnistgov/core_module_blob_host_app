3247e4f2db15051729bef27bca979cef981f07ba
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:27 -0400

    fix: revert django upgrade
    Change-Id: Icae38450811657a0be34f35b3950a7c61ab1b477

--- core_module_blob_host_app/urls.py
+++ core_module_blob_host_app/urls.py
@@ -1,10 +1,9 @@
 """ Url router for the blob host module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_blob_host_app.views.views import BlobHostModule
 
 urlpatterns = [
-    re_path(r'module-blob-host', BlobHostModule.as_view(), name='core_module_blob_host_view'),
+    url(r'module-blob-host', BlobHostModule.as_view(), name='core_module_blob_host_view'),
 ]
