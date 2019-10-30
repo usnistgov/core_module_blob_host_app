8c4852c8f381637294d2c02bb92cdbcb0da2fb40
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:26 -0400

    feat: upgrade django 2.2
    Change-Id: I2093c2cb6ec7995e0caa823a571685a44e6ee0c5

--- core_module_blob_host_app/urls.py
+++ core_module_blob_host_app/urls.py
@@ -1,9 +1,10 @@
 """ Url router for the blob host module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_blob_host_app.views.views import BlobHostModule
 
 urlpatterns = [
-    url(r'module-blob-host', BlobHostModule.as_view(), name='core_module_blob_host_view'),
+    re_path(r'module-blob-host', BlobHostModule.as_view(), name='core_module_blob_host_view'),
 ]
